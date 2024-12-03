dff2f7360ebb9d04a1ed407802e0d6c66697d600
pbtruong <tphanba@gmail.com>
2024-12-03 10:02:11 +0700

    studentFragment

---
+++ StudentFragment/app/src/main/java/com/example/studentmanagement/database/StudentData.kt
@@ -0,0 +1,18 @@
+package com.example.studentmanagement.database
+
+import com.example.studentmanagement.model.Student
+object StudentData {
+    val students = mutableListOf(
+        Student("Phạm Gia Bảo", "20305601"),
+        Student("Ngô Thanh Mai", "20305602"),
+        Student("Trịnh Văn Long", "20305603"),
+        Student("Đinh Ngọc Lan", "20305604"),
+        Student("Lương Hoàng Anh", "20305605"),
+        Student("Hồ Minh Khang", "20305606"),
+        Student("Nguyễn Thị Thanh Trúc", "20305607"),
+        Student("Đỗ Đức Huy", "20305608"),
+        Student("Trần Thị Mỹ Linh", "20305609"),
+        Student("Vũ Quang Duy", "20305610"),
+        Student("Lê Minh Tú", "20305611")
+    )
+}
